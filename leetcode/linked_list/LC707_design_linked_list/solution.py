be3e1aa575a2e5d7b445af3998ae1e173dd3cadd
Giiiinee <2412474657@qq.com>
2025-08-21 00:03:52 +0800

    docs:增加更简便方法

--- leetcode/linked_list/LC707_design_linked_list/solution.py
+++ leetcode/linked_list/LC707_design_linked_list/solution.py
@@ -1,4 +1,55 @@
 #LC707设计链表
+
+#法一(类方法之间的调用不要求严格顺序)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.size = 0
+        self.head = ListNode(0)  # 虚拟头结点
+
+    def get(self, index):
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for _ in range(index + 1):  # 多走一步到真正节点
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val):
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val):
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index,val):
+        if index > self.size:
+            return
+        if index < 0:
+            index = 0
+        self.size += 1
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        to_add = ListNode(val)
+        to_add.next = pred.next
+        pred.next = to_add
+
+    def deleteAtIndex(self, index):
+        if index < 0 or index >= self.size:
+            return
+        self.size -= 1
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        pred.next = pred.next.next
+
+
+#法二
 class ListNode:
     def __init__(self,val=0,next=None):
         self.val=val
