4edae6531adaee1959c9ff3a0564e94ef965215f
Giiiinee <2412474657@qq.com>
2025-12-08 12:49:50 +0800

    Create 1.py

---
+++ lanqiao/baifenbijiexi/1/1.py
@@ -0,0 +1,12 @@
+#百分比解析
+import pandas as pd
+study=pd.read_csv(r"D:\文件\py\pythonProject1\蓝桥杯\百分比解析\1\1.csv")
+col=study['learn_process'].astype(str)
+study['learn_pct']=col.str.extract(r'(\d+)')
+print(study[['learn_process','learn_pct']])
+invalid_count=study['learn_pct'].isna().sum()
+with open("task0102_2.txt","w") as f:
+    f.write(str(invalid_count))
+study_clean=study.dropna(subset=['learn_pct'])
+study_clean.loc[:,'learn_pct']=study_clean['learn_pct'].astype(int)
+study_clean.to_csv("study2_new.csv",index=False)
