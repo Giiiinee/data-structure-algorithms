1a6ca767bce0a5860f7cb73383fec950dcd8ec2c
Giiiinee <2412474657@qq.com>
2025-12-08 23:10:31 +0800

    Create 1.py

---
+++ lanqiao/quchong/1/1.py
@@ -0,0 +1,6 @@
+#去重
+import pandas as pd
+users=pd.read_csv(r"D:\文件\py\pythonProject1\蓝桥杯\去重\1\1.csv")
+users_sorted=users.sort_values(by='course_join_time',ascending=False)
+users_dup=users.drop_duplicates(subset=['user_id','course_id'],keep='first')
+users.to_csv("clean_study_dup.csv",index=False)
